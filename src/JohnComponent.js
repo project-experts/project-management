7aee16785e3e5255bd056681e41a2d29d4b10834
John Bozarov <codinginusa@gmail.com>
2020-03-06 11:46:23 -0700

    john crated component

---
+++ src/JohnComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class JohnComponent extends Component {
+   render() {
+      return (
+         <div>
+            John created this Component 
+         </div>
+      )
+   }
+}
+
+export default JohnComponent
