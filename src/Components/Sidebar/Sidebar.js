7000b3844d0ad37c5de31e18c226996fe29a81a0
Chris Jeppsen <chrisjeppsen@gmail.com>
2020-03-11 14:08:04 -0600

    lots of changes

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import { sidebarToggle } from '../../redux/reducers/sidebarReducer'
 import {userLoggedIn} from '../../redux/reducers/userReducer'
-import { toggle } from '../../redux/reducers/loginReducer'
 import { connect } from 'react-redux'
 import './Sidebar.css'
 import { AiOutlineLogout } from 'react-icons/ai'
 import {withRouter} from 'react-router-dom'
+import axios from 'axios'
 
 
 
@@ -26,20 +26,24 @@ componentDidUpdate(prev){
       })
    }
 }
-    render() {
-      
-       
-console.log(this.props)
 
+handleLogout = () => {
+   axios.post('/api/logout').then(res => {
+      this.props.sidebarToggle(!this.props.toggleSideBar)
+      this.props.history.push('/')
+   })
+}
+    render() {
       return (
-   
             <div className={this.props.toggleSideBar ? 'show_side_bar' : 'hide_side_bar'}>
+               <div id='profile_img_container'> 
                <div id='profile_img'>{this.state.userFirst} </div> 
+               </div>
 
                <div onClick={() => this.props.history.push('/dashboard')}className='sidebar_links'>Personal Dashboard</div>
                <div onClick={() => this.props.history.push('/charts')}className='sidebar_links'>Personal Stats Chart</div>
                <div onClick={() => this.props.history.push('/projects')}className='sidebar_links'>Projects List</div>
-               <AiOutlineLogout> </AiOutlineLogout>
+               <div id='logout_container'> <AiOutlineLogout id='logout'onClick={() => this.handleLogout()}> </AiOutlineLogout></div>
             </div>
       )  
             
