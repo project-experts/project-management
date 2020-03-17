7902748e2ffc8fb193f5ce4af31d5deff98bdde3
Leah Zhao <leahtingxuezhao@gmail.com>
2020-03-17 16:58:57 -0600

    landing page and header css

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,80 +1,133 @@
 import React, { Component } from "react";
-import styled from 'styled-components'
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
-import { loginClicked } from '../../redux/reducers/loginReducer'
-import { registerClicked } from '../../redux/reducers/registerReducer'
-import { sidebarToggle } from '../../redux/reducers/sidebarReducer'
-import { IoMdLogIn } from 'react-icons/io'
-import { FiUserCheck } from 'react-icons/fi'
-import { GiHamburgerMenu } from 'react-icons/gi'
-import logo from './logo.png'
+import styled from "styled-components";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { loginClicked } from "../../redux/reducers/loginReducer";
+import { registerClicked } from "../../redux/reducers/registerReducer";
+import { sidebarToggle } from "../../redux/reducers/sidebarReducer";
+import { IoMdLogIn } from "react-icons/io";
+import { FiUserCheck } from "react-icons/fi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import logo from "./logo.svg";
 import "./Header.css";
-import {userLoggedIn} from '../../redux/reducers/userReducer'
-
+import { userLoggedIn } from "../../redux/reducers/userReducer";
 
 class Header extends Component {
-   constructor(props){
-     super(props)
+  constructor(props) {
+    super(props);
 
-     this.state = {
-       
-     }
-   }
+    this.state = {};
+  }
 
-     
-   render(){
-      console.log(this.props)
-   return (
+  render() {
+    console.log(this.props);
+    return (
       <div>
-      {!this.props.user.first_name  ?  (
-         <div className="header">
-         <div></div>
-         <img src={logo} style={{width: '30px', height: '30px', cursor: 'pointer'}} onClick={() => this.props.history.push('/')} />
-         <div class="input-field col s6">
-            <input placeholder="Search" id="first_name" type="text" class="validate"/>
-         </div>
-         <StyledNav  >
-         <span>Contact us</span>
-         <span>Meet with the team</span>
-         <div><IoMdLogIn size={20} onClick={() => this.props.loginClicked(true)} ></IoMdLogIn></div>
-         <div><FiUserCheck size={20} onClick={() => this.props.registerClicked(true)} ></FiUserCheck></div>
-         </StyledNav>
-         </div>
-         
-      
-      ) : ( 
-         <div className="header">
-         <GiHamburgerMenu size={17} style={{color: 'black'}}  onClick={() => this.props.sidebarToggle(!this.props.toggleSideBar)}></GiHamburgerMenu>
-         <img src={logo} style={{width: '30px', height: '30px', cursor: 'pointer'}} onClick={() => this.props.history.push('/')} />
-         <div class="input-field col s6">
-            <input placeholder="Search" id="first_name" type="text" class="validate"/>
-         </div>
-         <StyledNav  >
-         <span>Contact us</span>
-         <span>Meet with the team</span>
-         <div><IoMdLogIn size={20} onClick={() => this.props.loginClicked(true)} ></IoMdLogIn></div>
-         <div><FiUserCheck size={20} onClick={() => this.props.registerClicked(true)} ></FiUserCheck></div>
-         </StyledNav>
-         </div>
-      )}
+        {!this.props.user.first_name ? (
+          <div className="header">
+            <div></div>
+            <img
+              src={logo}
+              style={{
+                width: "110px",
+                height: "55px",
+                cursor: "pointer",
+                marginLeft: "-120px"
+              }}
+              onClick={() => this.props.history.push("/")}
+            />
+            <div class="input-field col s6">
+              <input
+                placeholder="Search"
+                id="first_name"
+                type="text"
+                class="validate"
+              />
+            </div>
+            <StyledNav>
+              <span>CONTACT US</span>
+              <span>MEET WITH THE TEAM</span>
+              <div>
+                <IoMdLogIn
+                  size={25}
+                  onClick={() => this.props.loginClicked(true)}
+                ></IoMdLogIn>
+              </div>
+              <div>
+                <FiUserCheck
+                  size={25}
+                  onClick={() => this.props.registerClicked(true)}
+                ></FiUserCheck>
+              </div>
+            </StyledNav>
+          </div>
+        ) : (
+          <div className="header">
+            <GiHamburgerMenu
+              size={17}
+              style={{ color: "black" }}
+              onClick={() =>
+                this.props.sidebarToggle(!this.props.toggleSideBar)
+              }
+            ></GiHamburgerMenu>
+            <img
+              src={logo}
+              style={{
+                width: "110px",
+                height: "55px",
+                cursor: "pointer",
+                marginLeft: "-120px"
+              }}
+              onClick={() => this.props.history.push("/")}
+            />
+            <div class="input-field col s6">
+              <input
+                placeholder="Search"
+                id="first_name"
+                type="text"
+                class="validate"
+              />
+            </div>
+            <StyledNav>
+              <span>CONTACT US</span>
+              <span>MEET WITH THE TEAM</span>
+              <div>
+                <IoMdLogIn
+                  size={25}
+                  onClick={() => this.props.loginClicked(true)}
+                ></IoMdLogIn>
+              </div>
+              <div>
+                <FiUserCheck
+                  size={25}
+                  onClick={() => this.props.registerClicked(true)}
+                ></FiUserCheck>
+              </div>
+            </StyledNav>
+          </div>
+        )}
       </div>
-   ) 
+    );
   }
 }
 
 function mapStateToProps(state) {
-   return {
-      toggleSideBar: state.sidebarReducer.toggleSideBar,
-      isLoginClicked: state.loginReducer.isLoginClicked,
-      isRegClicked: state.registerReducer.isRegClicked,
-      user: state.userReducer.user
-   }
+  return {
+    toggleSideBar: state.sidebarReducer.toggleSideBar,
+    isLoginClicked: state.loginReducer.isLoginClicked,
+    isRegClicked: state.registerReducer.isRegClicked,
+    user: state.userReducer.user
+  };
 }
-export default connect(mapStateToProps, { loginClicked, registerClicked, sidebarToggle, userLoggedIn })(withRouter(Header));
+export default connect(mapStateToProps, {
+  loginClicked,
+  registerClicked,
+  sidebarToggle,
+  userLoggedIn
+})(withRouter(Header));
 
 const StyledNav = styled.nav`
-   width: 50%;
-   display: flex; 
-   justify-content: space-between;
-`; 
+  width: 50%;
+  display: flex;
+  justify-content: space-around;
+`;
