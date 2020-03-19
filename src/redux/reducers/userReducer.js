a97c4dc25a6f4c28150547c71816cccc481f63d1
John Bozarov <codinginusa@gmail.com>
2020-03-19 19:03:28 -0400

    Hamburger, teammembers on Projects list

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -13,18 +13,18 @@ export const userLoggedIn = user => {
 }
 
 
-//We uncomment it when we set up log out
-   // export const userLoggedOut = () => {
-   //    return {
-   //       type: USER_LOGGED_OUT, 
-   //       payload: {}
-   //    }
-   // }
+export const userLoggedOut = () => {
+   return {
+      type: USER_LOGGED_OUT, 
+      payload: {}
+   }
+}
 
 export default function userReducer(state = initialState, action) {
    const { type, payload } = action; 
    switch(type) {
-      case USER_LOGGED_IN : return {...state, user: payload }
+      case USER_LOGGED_IN : return {...state, user: payload }; 
+      case USER_LOGGED_OUT : return {...state, user: {}}; 
       default: return state; 
    }
 }
